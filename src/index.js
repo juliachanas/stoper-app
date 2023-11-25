c5118733c23fcfb01413d42dcfb9df7e6cf627d4
Julia Chanas <juulia.chanas@gmail.com>
2023-11-25 18:37:39 +0100

    Remove componenr RenderTime.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './styles/normalize.scss';
 import './styles/global.scss';
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
